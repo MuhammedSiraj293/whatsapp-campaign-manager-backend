2e6e7d6cdc66638c9446be112ca1561381d801db
MuhammedSiraj293 <muhammedsiraj293@gmail.com>
2025-09-03 15:25:30 +0400

    short description

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const connectDB = require('./config/db');
 const campaignRoutes = require('./routes/campaignRoutes');
 const recipientRoutes = require('./routes/recipientRoutes');
 const webhookRoutes = require('./routes/webhookRoutes');
-const replyRoutes = require('./routes/replyRoutes'); // <-- IMPORT NEW ROUTES
+const replyRoutes = require('./routes/replyRoutes');
+const authRoutes = require('./routes/authRoutes'); // <-- IMPORT NEW ROUTES
 
 dotenv.config();
 connectDB();
@@ -27,7 +28,8 @@ app.get('/', (req, res) => {
 app.use('/api/campaigns', campaignRoutes);
 app.use('/api/recipients', recipientRoutes);
 app.use('/api/webhook', webhookRoutes);
-app.use('/api/replies', replyRoutes); // <-- USE THE NEW ROUTES
+app.use('/api/replies', replyRoutes);
+app.use('/api/auth', authRoutes); // <-- USE THE NEW ROUTES
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
