4e7c5eea8cdd9e0b62251959c7159f55f567fade
MuhammedSiraj293 <muhammedsiraj293@gmail.com>
2025-09-14 16:11:22 +0400

    first commit

--- src/routes/mediaRoutes.js
+++ src/routes/mediaRoutes.js
@@ -1,12 +1,12 @@
 // backend/src/routes/mediaRoutes.js
-    
+
 const express = require('express');
 const { getMediaFile } = require('../controllers/mediaController');
-const { protect } = require('../middleware/authMiddleware');
+// We are removing 'protect' because the <img> tag cannot send an auth token.
 
 const router = express.Router();
-    
-// This route will take a media ID and proxy the file from Meta
-router.get('/:mediaId', protect, getMediaFile);
-    
+
+// This route is now public so that the browser's <img> tag can access it.
+router.get('/:mediaId', getMediaFile);
+
 module.exports = router;
